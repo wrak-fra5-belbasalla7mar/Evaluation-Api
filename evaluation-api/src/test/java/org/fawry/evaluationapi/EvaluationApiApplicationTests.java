27b565c36b58b496411f2db25256b5639ae70406
jihad wael <144389076+JihadWael099@users.noreply.github.com>
2025-03-14 21:31:20 +0200

    init commit

---
+++ evaluation-api/src/test/java/org/fawry/evaluationapi/EvaluationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fawry.evaluationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
